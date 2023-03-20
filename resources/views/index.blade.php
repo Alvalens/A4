3904ea9c5ef0e5fda18747f712a5bb0d2799ae0d
Alvalen <alvalen.shafel04@gmail.com>
2023-03-20 18:54:22 +0700

    fix index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,209 +1,143 @@
 @extends('layout.master')
-@section('title', 'Yuk, Belajar!')
+@section('title', 'Selamat datang!')
 @section('css', 'index')
 
-@section('content') 
+@section('content')
 
-<body style="overflow-x: hidden">
-  <section data-bss-parallax-bg="true" style="background: url('pictures/bgi.jpg'); background-size: cover;" class="vh-100">
-    {{-- clouds --}}
-    <div class="clouds">
-      <div class="container-fluid">
-        {{-- c1 --}}
-        <div class="row mb-3">
-          <div class="col p-0">
-            <div class="card cardf">
-            </div>
-          </div>
-          <div class="col">
-            <div class="card cardf">
-            </div>
-          </div>
-          <div class="col p-0">
-            <div class="card cardf">
-            </div>
-          </div>
-        </div>
-        {{-- c2 --}}
-        <div class="row mb-2">
-          <div class="col p-0">
-            <div class="card cardf2" style="margin-left: -15px">
-            </div>
-          </div>
-          <div class="col">
-          </div>
-          <div class="col p-0 d-flex flex-column justify-content-end align-items-end">
-            <div class="card cardf2 " style="margin-right: -15px">
-            </div>
-          </div>
-        </div>
-        {{-- c3 --}}
-        <div class="row mb-2">
-          <div class="col p-0">
-            <div class="card cardf3" style="margin-left: -20px">
-            </div>
-          </div>
-          <div class="col">
-          </div>
-          <div class="col p-0 d-flex flex-column justify-content-end align-items-end">
-            <div class="card cardf3 " style="margin-right: -20px">
-            </div>
-          </div>
-        </div>
-        {{-- c4 --}}
+  <body style="overflow-x: hidden">
+    <section data-bss-parallax-bg="true" style="background: url('assets/img/bg-sky.jpg'); background-size: cover;"
+      class="vh-100" class="img-hero2">
+      {{-- main  --}}
+      <div class="container d-flex flex-column justify-content-center align-items-center h-100">
         <div class="row">
-          <div class="col p-0">
-            <div class="card cardf4" style="margin-left: -15px">
+          <div class="col-md-6">
+            {{-- gambar --}}
+            <div class="img text-start ext-md-start" >
+              <img src="{{ url('assets/img/h1.png') }}" alt="" class="img-fluid"
+                style="max-height:400px; max-width:400px;">
+            </div>
+            {{-- sub h --}}
+            <div class="subh">
+              <p class="text-start">(Aksesibilitas, Atraktif, Antusias, Akademik)</p>
+            </div>
+            {{-- desc --}}
+            <div class="desc p-0 m-0 text-start text-md-start">
+              <p>Platform media pembelajaran interaktif dengan game-based education dan trivia berbasis website untuk anak
+                tunagrahita ringan, memiliki mengedepankan pada aksesibiltas, atraktif, dan antusias akademik.</p>
+            </div>
+            {{-- button --}}
+            <div class="button text-start text-md-start">
+              <a href="lojin" type="button" class="buthon mx-3"> Mulai!</a>
+              <a href="/about" type="button" class="buthon2"> lebih lanjut</a>
+
             </div>
           </div>
+          <div class="col-md-6">
+            {{-- main img --}}
+            <div class="img-main text-center">
+              <img src="{{ url('pictures/pensilterbang.png') }}" alt="pensil" draggable="false" class="img-fluid"
+                style="height:700px; ">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    {{-- create a fixed button in the middle bottom functionm to scroll down --}}
+    <div class=" text-center">
+      <a class="btn-down">
+        <i class="fa-solid fa-arrow-down"></i>
+      </a>
+    </div>
+
+    {{-- hal 2 --}}
+    <section style="background: url('pictures/bg.jpg'); background-size: cover;" class="vh-100 d-flex" id="section2">
+      {{-- main --}}
+      <div class="container d-flex flex-column justify-content-center align-items-center h-100">
+        <div class="row d-flex flex-column justify-content-center align-items-center">
+          {{-- head --}}
+          <img src="{{ url('assets/img/h11.png') }}" alt="" class="img-fluid img-sec">
+          <img src="assets/img/geser.png" alt="" class="img-fluid img-sec2">
+          {{-- carousel  --}}
           <div class="col">
-          </div>
-          <div class="col p-0 d-flex flex-column justify-content-end align-items-end">
-            <div class="card cardf4" style="margin-right: -15px">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="{{ url('assets/img/belajar1.png') }}" class="img-fluid text-center img-c" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="{{ url('assets/img/bermain1.png') }}" class="img-fluid text-center img-c" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="{{ url('assets/img/tekateki1.png') }}" class="img-fluid text-center img-c" alt="...">
+                </div>
+              </div>
+              {{-- btn --}}
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                data-bs-slide="prev">
+                <span class="carousel-control-prevs-icon" style="font-size: 8rem" aria-hidden="true"><i class="fa-solid fa-chevron-left"></i>
+                  </span>
+                    <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                data-bs-slide="next">
+                <span class="carousel-control-nexst-icon" style="font-size: 8rem"aria-hidden="true"><i class="fa-solid fa-chevron-right"></i></span>
+                <span class="visually-hidden">Next</span>
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    {{-- main  --}}
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          {{-- gambar --}}
-          <div class="img text-center">
-            <img src="{{ url('pictures/h1.png') }}" alt="" class="img-fluid" style="max-height:400px;">
-          </div>
-          {{-- sub h --}}
-          <div class="subh">
-            <p class="text-center">(Aksesibilitas, Atraktif, Antusias, Akademik)</p>
-          </div>
-          {{-- desc --}}
-          <div class="desc px-md-5 mx-md-5 p-0 m-0">
-            <p>Platform media pembelajaran interaktif dengan game-based education dan trivia berbasis website untuk anak
-              tunagrahita ringan, memiliki mengedepankan pada aksesibiltas, atraktif, dan antusias akademik.</p>
-          </div>
-          {{-- button --}}
-          <div class="button text-center">
-            <a href="lojin" type="button" class="buthon mx-3"> Mulai!</a>
-            <a href="/about" type="button" class="buthon2"> lebih lanjut</a>
 
-          </div>
-        </div>
-        <div class="col-md-6">
-          {{-- main img --}}
-          <div class="img-main text-center">
-            <img src="{{ url('pictures/pensilterbang.png') }}" alt="pensil" draggable="false" class="img-fluid" style="height:700px; ">
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  {{-- create a fixed button in the middle bottom functionm to scroll down --}}
-<div class=" text-center">
-  <a class="btn-down">
-    <i class="fa-solid fa-arrow-down"></i>
-  </a>
-</div>
+    </section>
 
-  {{-- hal 2 --}}
+    {{-- script --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    {{-- jq --}}
+    <script src="{{ url('js/jq.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    {{-- paralax --}}
+    <script src="{{ url('js/parallax.js') }}"></script>
+    <script>
+      // hide navbar, then reveal on scroll
+      // $(document).ready(function() {
+      //   var lastScrollTop = 0;
+      //   $(window).scroll(function() {
+      //     var scrollTop = $(this).scrollTop();
+      //     if (scrollTop > lastScrollTop) {
+      //       $('#navbar').slideDown('slow');
 
-  <section style="background: url('pictures/bg.jpg'); background-size: cover;"
-    class="vh-100 d-flex" id="section2">
-    {{-- nav --}}
-      <nav id="navbar" class="navbar fixed-top">
-        <ul class="menu">
-          <li><a href="#services"><img src="pictures/navbar/beranda.png" alt="Home"></a></li>
-          <li><a href="#services"><img src="pictures/navbar/belajar.png" alt="Home"></a></li>
-          <li><a href="#services"><img src="pictures/navbar/bermain.png" alt="Home"></a></li>
-          <li><a href="#services"><img src="pictures/navbar/teka-teki.png" alt="Home"></a></li>
-        </ul>
-      </nav>
-      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-    {{-- main --}}
-<div class="container d-flex flex-column justify-content-center align-items-center h-100">
-  <div class="row d-flex flex-column justify-content-center align-items-center" >
-    {{-- head --}}
-    <img src="{{ url('pictures/h11.png') }}" alt="" class="img-fluid">
-    {{-- carousel  --}}
-    <div class="col">
-      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="{{ url('pictures/1.png') }}" class="img-fluid text-center" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="{{ url('pictures/1.png') }}" class="img-fluid text-center" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="{{ url('pictures/1.png') }}" class="img-fluid text-center" alt="...">
-          </div>
-        </div>
-        {{-- btn --}}
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-          data-bs-slide="prev">
-          <span class="carousel-control-prevs-icon" style="font-size: 8rem" aria-hidden="true"><</span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-          data-bs-slide="next">
-          <span class="carousel-control-nexst-icon" style="font-size: 8rem"aria-hidden="true">></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
-  </div>
-</div>
+      //     } else {
+      //       $('#navbar').slideUp('slow');
+      //     }
+      //     lastScrollTop = scrollTop;
+      //   });
+      // });
 
-  </section>
+      // hide btn-down on scroll
+      $(document).ready(function() {
+        var lastScrollTop = 0;
+        $(window).scroll(function() {
+          var scrollTop = $(this).scrollTop();
+          if (scrollTop > lastScrollTop) {
+            $('.btn-down').fadeOut('slow');
 
-  {{-- script --}}
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  {{-- jq --}}
-  <script src="{{ url('js/jq.js') }}"></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  {{-- paralax --}}
-  <script src="{{ url('js/parallax.js') }}"></script>
-  <script>
-    // hide navbar, then reveal on scroll
-    $(document).ready(function() {
-      var lastScrollTop = 0;
-      $(window).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > lastScrollTop) {
-          $('#navbar').slideDown('slow');
+          } else {
+            $('.btn-down').fadeIn('slow');
 
-        } else {
-          $('#navbar').slideUp('slow');
-        }
-        lastScrollTop = scrollTop;
+          }
+          lastScrollTop = scrollTop;
+        });
       });
-    });
+      // make btn-down scroll to the next section
+      const btnDown = $('.btn-down');
+      const section2 = document.getElementById('section2');
 
-    // hide btn-down on scroll
-    $(document).ready(function() {
-      var lastScrollTop = 0;
-      $(window).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > lastScrollTop) {
-           $('.btn-down').fadeOut('slow');
-
-        } else {
-$('.btn-down').fadeIn('slow');
-
-        }
-        lastScrollTop = scrollTop;
+      btnDown.on('click', () => {
+        section2.scrollIntoView({
+          behavior: 'smooth'
+        });
       });
-    });
-    // make btn-down scroll to the next section
-    const btnDown = $('.btn-down');
-    const section2 = document.getElementById('section2');
+    </script>
 
-  btnDown.on('click', () => {
-    section2.scrollIntoView({ behavior: 'smooth' });
-  });
-
-  </script>
-
-@endsection
+  @endsection
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,79 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    
-    <!-- Title -->
-    <title>@yield('title', 'Beranda')</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+  <!-- Title -->
+  <title>@yield('title', 'Beranda')</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-    <!-- Vendor CSS Files -->
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-    
-    <!-- Template Main CSS File -->
-    <link href="assets/css/main.css" rel="stylesheet">
-    <link href="assets/css/@yield('css', 'main').css" rel="stylesheet">
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet">
 
-    <!-- Make Body CSS Style -->
-    <style>
-        body {
-            @yield('body-style')
-        }
-    </style>
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="assets/css/@yield('css', 'main').css" rel="stylesheet">
+
+  {{-- icon --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+    integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!-- Make Body CSS Style -->
+  <style>
+      #navbar {
+    transition: top 0.3s ease-in-out;
+  }
+  .navbar {
+    background-color: #add8e683;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  }
+  #navbar.navbar-hide {
+    top: -100px;
+  }
+    body {
+      @yield('body-style')
+    }
+  </style>
 
 </head>
+
 <body>
-        
-    <!-- ======= Header/Navbar Section ======= -->
-    <!-- Start Header -->
-    <header id="header" class="header d-flex align-items-center">
-      <div class="container-fluid container-xl d-flex align-items-center justify-content-center">
-        <nav id="navbar" class="navbar navbar-hidden">
-          <ul class="menu">
-            <li><a href="#services"><img src="assets/img/navbar/beranda.png" alt="Home"></a></li>
-            <li><a href="#services"><img src="assets/img/navbar/belajar.png" alt="Home"></a></li>
-            <li><a href="#services"><img src="assets/img/navbar/bermain.png" alt="Home"></a></li>
-            <li><a href="#services"><img src="assets/img/navbar/teka-teki.png" alt="Home"></a></li>
-          </ul>
-        </nav>  
-        <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-        <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-      </div>
-    </header>
-    <!-- End Header -->
-  
-  
-  
-  
+  {{-- navbar s --}}
+  <nav id="navbar" class="navbar fixed-top">
+    <ul class="menu">
+      <li><a href="#services"><img src="pictures/navbar/beranda.png" alt="Home"></a></li>
+      <li><a href="#services"><img src="pictures/navbar/belajar.png" alt="Home"></a></li>
+      <li><a href="#services"><img src="pictures/navbar/bermain.png" alt="Home"></a></li>
+      <li><a href="#services"><img src="pictures/navbar/teka-teki.png" alt="Home"></a></li>
+    </ul>
+  </nav>
   @section('content')
-  
+
   @show
-  
-  
-  
-  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-    
+
+
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
   <div id="preloader"></div>
-  
-      <footer>
-        <div class="text-center p-3">
-          © 2023 Copyright:
-          <p><strong>A4 Team | S1 TI A | Universitas Negeri Malang</strong></p>
-        </div>
-        <!-- Copyright -->
-      </footer>
-  
+
+  <footer>
+    <div class="text-center p-3">
+      © 2023 Copyright:
+      <p><strong>A4 Team | S1 TI A | Universitas Negeri Malang</strong></p>
+    </div>
+    <!-- Copyright -->
+  </footer>
+
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/aos/aos.js"></script>
@@ -82,10 +89,27 @@
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
-  
+
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
   <script src="assets/js/@yield('js', 'main').js"></script>
-  
-  </body>
-  </html>
+
+  <script>
+          
+      $(document).ready(function() {
+        var lastScrollTop = 0;
+        $(window).scroll(function() {
+          var scrollTop = $(this).scrollTop();
+          if (scrollTop > lastScrollTop) {
+            $('#navbar').slideDown('slow');
+
+          } else {
+            $('#navbar').slideUp('slow');
+          }
+          lastScrollTop = scrollTop;
+        });
+      });
+  </script>
+</body>
+
+</html>
