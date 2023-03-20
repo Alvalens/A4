20c573e24f132cb2499aa603ca1a99a285639133
Adin.dXa <113296209+00038133000@users.noreply.github.com>
2023-03-20 11:31:29 +0700

    banyak

---
+++ public/assets/js/belajar.js
@@ -0,0 +1,60 @@
+function toggleFormDone1() {
+    var feedbackDiv = document.querySelector(".feedback1");
+    if (feedbackDiv.style.display === "none") {
+      feedbackDiv.style.display = "block";
+    } else {
+      feedbackDiv.style.display = "none";
+    }
+  }
+
+function toggleFormDone2() {
+    var feedbackDiv = document.querySelector(".feedback2");
+    if (feedbackDiv.style.display === "none") {
+      feedbackDiv.style.display = "block";
+    } else {
+      feedbackDiv.style.display = "none";
+    }
+  }
+
+function toggleFormDone3() {
+    var feedbackDiv = document.querySelector(".feedback3");
+    if (feedbackDiv.style.display === "none") {
+      feedbackDiv.style.display = "block";
+    } else {
+      feedbackDiv.style.display = "none";
+    }
+  }
+  
+function toggleFormDone4() {
+    var feedbackDiv = document.querySelector(".feedback4");
+    if (feedbackDiv.style.display === "none") {
+      feedbackDiv.style.display = "block";
+    } else {
+      feedbackDiv.style.display = "none";
+    }
+  }
+  
+function toggleFormDone5() {
+    var feedbackDiv = document.querySelector(".feedback5");
+    if (feedbackDiv.style.display === "none") {
+      feedbackDiv.style.display = "block";
+    } else {
+      feedbackDiv.style.display = "none";
+    }
+  }
+  
+function toggleFormDone6() {
+    var feedbackDiv = document.querySelector(".feedback6");
+    if (feedbackDiv.style.display === "none") {
+      feedbackDiv.style.display = "block";
+    } else {
+      feedbackDiv.style.display = "none";
+    }
+  }
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function(event) {
+  event.preventDefault(); // prevent the default behavior of the click event
+  // your code to handle the button click goes here
+});
