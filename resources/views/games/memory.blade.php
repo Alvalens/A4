091cc102300b0239dacf1175faef2276efb727ac
Alvalen <alvalen.shafel04@gmail.com>
2023-05-19 09:00:11 +0700

    games rapi

--- resources/views/games/memory.blade.php
+++ resources/views/games/memory.blade.php
@@ -1,39 +1,30 @@
 @extends('layout.master')
 @section('title', 'Bermain itu Seru!')
 
-
-@section('content')
+@section('css')
+  <link rel="stylesheet" href="{{ url('assets/css/bermain.css') }}">
   <link rel="stylesheet" href="{{ url('assets/css/games/memory.css') }}">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Coda">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/sweetalert2/3.0.3/sweetalert2.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css">
+
   <style>
-    .tombol-terbang {
-      position: fixed;
-      bottom: 50px;
-      right: 20px;
-      width: 50px;
-      height: 50px;
-      border-radius: 50%;
-      background-color: #2872b8;
-      color: #fff;
-      font-size: 20px;
-      text-align: center;
-      line-height: 50px;
-      box-shadow: 0 0 6px rgba(0, 0, 0, 0.329);
-      transition: all 0.3s ease;
-      z-index: 99999;
-    }
-    .fa-reply {
-      margin-top: 16px;
+    /* when on md and lower scale section to 0.5 */
+    @media (max-width: 991px) {
+      section {
+        transform: scale(0.5);
+      }
     }
   </style>
-  <section>
-      <div class="tombol">
+@endsection
+
+@section('content')
+  <div class="tombol">
     <a href="{{ url()->previous() }}" class="tombol-terbang" role="button" type="button">
       <i class="fa-solid fa-reply"></i></a>
   </div>
+  <section>
     <div id="score-panel">
       <ul class="stars">
         <li><i class="fa fa-star"></i></li>
